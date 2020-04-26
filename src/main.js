c7968b8f15803f911dee0eca9f9f5abf63e6e113
nakajima1412 <nakajima1412@icloud.com>
2020-04-27 00:49:45 +0900

    不正トークン時にログアウトする処理を追加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -14,6 +15,15 @@ new Vue({
       const userData = JSON.parse(userString)
       this.$store.commit('SET_USER_DATA', userData)
     }
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
   },
   render: h => h(App)
 }).$mount('#app')
